513df7f32a821491f1c9e74c607cb47635d1b146
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2019-09-15 19:08:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -40,7 +40,7 @@ client.on('message', message => {
         message.guild.channels.delete
     }
 
-    if (message.content.toLowerCase().startsWith(prefix + "test")) {
+    if (message.content.toLowerCase().startsWith(prefix + "ticket")) {
         const reason = message.content.split(" ").slice(1).join(" ");
         if (!message.guild.roles.exists("name", "tickmanager")) return message.channel.send(`This server doesn't have a tickmanager role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`Tu as deja une demmande en cours !`);
@@ -60,17 +60,7 @@ client.on('message', message => {
           .setTitle("Tu vas bientot recevoir ton nito !!!")
           .setDescription("Merci de patienter et de ne toucher a rien")
           message.react('✅')
-          channel.send(embed)
-  
-          var args = message.content.split(" ").slice(1);
-
-      var embed = new Discord.RichEmbed()
-      .setTitle("Merci | Thank you " + message.author.username + "#" + message.author.discriminator)
-      .addField("Invite moi ", "[Clique ici pour être redirigé](https://discordapp.com/oauth2/authorize?client_id=606181494125494273&scope=bot&permissions=2146958847)")
-      .addField("Merci d'avoir utiliser le support , votre requette est en attentes , un administrateur vous contacteras via ce salon \n \n \n Thank you for using the support, your request is pending, an administrator will contact you through this show")
-      .setThumbnail(message.author.avatarURL)
       c.send(embed)
-      c.send("quel est votre question ??? \n \n \n What's your question???")
 });
 
 
